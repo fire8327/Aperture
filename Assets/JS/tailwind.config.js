ad6baa37630a63b8f734634c4bd17e27423b27b2
fire8327 <fire83274@gmail.com>
2022-12-09 23:45:23 +0300

    footer last

--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -1,8 +1,18 @@
-module.exports = {
-    content: [],
-    theme: {
-      colors: {},  
-      extend: {},
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {            
+          'header': '#181818',
+          'silver':'#C0C0C0'
+        },
     },
-    plugins: [],
-}
+    screens: {
+      'ml':'425px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
+};
